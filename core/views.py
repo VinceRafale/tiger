84dc6379b24c2fc15b012a88284bf2467b87a09f
Jonathan Lukens <jonathan@threadsafelabs.com>
2009-12-25 14:05:35 -0500

    Fixed retarded mistake w/ return type.

--- core/views.py
+++ core/views.py
@@ -1,22 +1,23 @@
+from django.http import HttpResponse
 from tiger.core.models import Section, Item
 from tiger.utils.views
 
 def section_list(request):
     sections = Section.objects.filter(site=request.site)
-    return render_custom(request, 'core/section_list.html', 
-        {'sections': sections})
+    return HttpResponse(render_custom(request, 'core/section_list.html', 
+        {'sections': sections}))
 
 def section_detail(request, section):
     s = Section.objects.get(slug=section, site=request.site)
-    return render_custom(request, 'core/section_detail.html', 
-        {'section': s})
+    return HttpResponse(render_custom(request, 'core/section_detail.html', 
+        {'section': s}))
 
 def item_detail(request, section, item):
     i = Item.objects.get(section__slug=section, slug=item, site=request.site)
     if request.method == 'POST':
         qty = request.POST.get('qty')
         request.cart.add(i.id, int(qty))
-    return render_custom(request, 'core/item_detail.html', 
-        {'item': i})
+    return HttpResponse(render_custom(request, 'core/item_detail.html', 
+        {'item': i}))
 
 
