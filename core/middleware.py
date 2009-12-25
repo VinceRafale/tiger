8f09737d7ab803290b5097a9b15b7f5a0aa10e56
Jonathan Lukens <jonathan@threadsafelabs.com>
2009-12-25 11:46:27 -0500

    Refactored cart and middleware to use separate sessions for each site.

--- core/middleware.py
+++ core/middleware.py
@@ -1,17 +1,24 @@
+import hashlib
+import time
+from datetime import time
+
+from django.conf import settings
+from django.contrib.sessions.models import Session
+
+from tiger.core.models import Item
+
 class Cart(object):
     contents = {}
-    model = None
 
-    def __get__(self, request, obj_type=None):
-        session = self._session = request.session
-        site = self._site = request.site
-        if hasattr(session, '_cart_contents'):
-            self.contents = session['_cart_contents']
-        else:
-            session['_cart_contents'] = {}
+    def __init__(self, session):
+        self.session = session
+        self.contents = session.get_decoded()
 
     def __iter__(self):
-        return iter((self.model.objects.get(id=k), v) for k, v in self.contents.items())
+        """Iterating over the cart returns a list of tuples consisting of
+        the Item in the cart and the corresponding quantity.
+        """
+        return iter((Item.objects.get(id=k), v) for k, v in self.contents.items())
 
     def __len__(self):
         return len(self.contents)
@@ -22,15 +29,18 @@ class Cart(object):
             contents[obj_id] = contents[obj_id] + qty
         else:
             contents[obj_id] = qty
-        self._session['_cart_contents'] = contents
+        self.session.session_data = Session.objects.encode(contents)
+        self.session.save()
 
     def remove(self, obj_id):
         contents = self.contents
         contents.pop(obj_id, None)
-        self._session['_cart_contents'] = contents
+        self.session.session_data = Session.objects.encode(contents)
+        self.session.save()
 
     def clear(self):
-        self._session['_cart_contents'] = {}
+        self.session.session_data = Session.objects.encode({})
+        self.session.save()
 
 
 class ShoppingCartMiddleware(object):
@@ -39,5 +49,21 @@ class ShoppingCartMiddleware(object):
     able to keep transactions for different sites separate.  
     """
     def process_request(self, request):
-        request.__class__.cart = Cart()
-    
+        cookie_name, session_key = self._get_cookie_pair(request)
+        if session_key:
+            s = Session.objects.get(session_key=session_key)
+            request.cart = Cart(s)
+
+    def process_response(self, request, response):
+        cookie_name, session_key = self._get_cookie_pair(request)
+        if not session_key:
+            session_key = hashlib.md5(cookie_name + settings.SECRET_KEY)
+            Session.objects.save(session_key, {}, date.today() + timedelta(days=7))
+            response.set_cookie(cookie_name, session_key)
+        return response
+
+    def _get_cookie_pair(self, request):
+        site = request.site
+        cookie_name = 'takeouttiger_%s' % site.domain
+        session_key = request.COOKIES.get(cookie_name)
+        return cookie_name, session_key
