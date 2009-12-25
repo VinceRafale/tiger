f9cfc5b15a945fc1798dd2047f40dad60364a760
Jonathan Lukens <jonathan@threadsafelabs.com>
2009-12-25 08:58:51 -0500

    Added beginnings of skinnable design architecture.

--- core/views.py
+++ core/views.py
@@ -1,10 +1,22 @@
+from tiger.core.models import Section, Item
+from tiger.utils.views
+
 def section_list(request):
-    pass
+    sections = Section.objects.filter(site=request.site)
+    return render_custom(request, 'core/section_list.html', 
+        {'sections': sections})
 
 def section_detail(request, section):
-    pass
+    s = Section.objects.get(slug=section, site=request.site)
+    return render_custom(request, 'core/section_detail.html', 
+        {'section': s})
 
 def item_detail(request, section, item):
-    pass
+    i = Item.objects.get(section__slug=section, slug=item, site=request.site)
+    if request.method == 'POST':
+        qty = request.POST.get('qty')
+        request.cart.add(i.id, int(qty))
+    return render_custom(request, 'core/item_detail.html', 
+        {'item': i})
 
 
--- urls.py
+++ urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
@@ -9,8 +10,12 @@ from tiger.core import site
 urlpatterns = patterns('',
     # Example:
     # (r'^tiger/', include('tiger.foo.urls')),
+    url(r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'base.html'}, name='home'),
     (r'^menu/', include('tiger.core.urls')),
     (r'^search/', include('haystack.urls')),
     (r'^controlpanel/', include(site.urls)),
     (r'^admin/', include(admin.site.urls)),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT}),
+
 )
