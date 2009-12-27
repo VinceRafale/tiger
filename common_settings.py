37eb23aafed87e9b90361f119b082f9d9fc30ae2
Jonathan Lukens <jonathan@threadsafelabs.com>
2009-12-26 21:20:16 -0500

    Added passing tests for DomainDetectionMiddleware.

--- common_settings.py
+++ common_settings.py
@@ -49,6 +49,15 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.load_template_source',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.contrib.messages.context_processors.messages',
+    'tiger.accounts.context_processors.site',
+)
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
