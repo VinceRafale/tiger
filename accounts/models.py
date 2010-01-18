b1f8772d4b20b744cade9527c0bb759cc87cd799
Jonathan Lukens <jonathan@threadsafelabs.com>
2010-01-17 23:35:29 -0500

    Fixed missing max_length on CharField.

--- accounts/models.py
+++ accounts/models.py
@@ -108,7 +108,7 @@ class Subscriber(models.Model):
     )
     user = models.ForeignKey(User)
     site = models.ForeignKey(Site)
-    organization = models.CharField()
+    organization = models.CharField(max_length=255)
     send_updates = models.BooleanField(default=True)
     update_via = models.IntegerField(default=VIA_EMAIL, choices=VIA_CHOICES)
     fax = PhoneNumberField(blank=True)
