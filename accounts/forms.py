caa66a865fc87746e12513473ccfb0b397a25e24
Jonathan Lukens <jonathan@threadsafelabs.com>
2010-01-18 08:06:03 -0500

    Added validation for fax/e-mail options.

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,3 +12,19 @@ class SubscriberForm(forms.ModelForm):
     class Meta:
         model = Subscriber
         exclude = ['user', 'site']
+
+    def clean(self):
+        super(SubscriberForm, self).clean()
+        cleaned_data = self.cleaned_data
+        send_updates = cleaned_data.get('send_updates')
+        if not send_updates:
+            return cleaned_data
+        update_via = cleaned_data['update_via']
+        if update_via == Subscriber.VIA_EMAIL and not cleaned_data.get('email'):
+            msg = "You must enter an e-mail address to use e-mail updates."
+            raise forms.ValidationError(msg)
+        if update_via == Subscriber.VIA_FAX and not cleaned_data.get('fax'):
+            msg = "You must enter a fax number to use fax updates."
+            raise forms.ValidationError(msg)
+        return cleaned_data
+            
