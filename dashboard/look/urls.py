6611b933d1cd66cfd7ffb07948e125c06854f4f6
Jonathan Lukens <jonathan@threadsafelabs.com>
2010-02-27 08:53:57 -0500

    Made it possible to save your skin choice.

--- dashboard/look/urls.py
+++ dashboard/look/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('tiger.dashboard.look.views',
     url(r'^$', 'picker', name='dashboard_look'),
+    url(r'^select-skin/$', 'select_skin', name='select_skin'),
 )
 
--- dashboard/look/views.py
+++ dashboard/look/views.py
@@ -1,8 +1,19 @@
+from django.http import HttpResponse
 from django.views.generic.simple import direct_to_template
 
 from tiger.look.forms import SkinSelectForm
+from tiger.look.models import Skin
 
 def picker(request):
     form = SkinSelectForm()
     return direct_to_template(request, template='dashboard/look/preview.html',
         extra_context={'form': form})
+
+
+def select_skin(request):
+    skin_id = request.POST.get('id')
+    skin = Skin.objects.get(id=skin_id)
+    site = request.site
+    site.skin = skin
+    site.save()
+    return HttpResponse("Your settings have been saved.")
