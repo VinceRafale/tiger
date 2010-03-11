746ade418e0f9dde3b4d8d6cdef6b4b16bf581d5
Jonathan Lukens <jonathan@threadsafelabs.com>
2010-03-11 07:04:59 -0500

    Added Whoosh path for prod environment.

--- prod_settings.py
+++ prod_settings.py
@@ -10,3 +10,4 @@ DATABASE_PASSWORD = 'MAAANNNIIING'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
+HAYSTACK_WHOOSH_PATH = '/home/threadsafe/whoosh_index'
