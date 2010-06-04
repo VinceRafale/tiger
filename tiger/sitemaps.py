fe5740ed2f6dba4b313ef41eaaa94147219968f0
Jonathan Lukens <jonathan@threadsafelabs.com>
2010-06-04 11:01:18 -0400

    Added news items to sitemaps.

--- tiger/sitemaps.py
+++ tiger/sitemaps.py
@@ -18,5 +18,10 @@ def sitemap(request):
         site_name + item.get_absolute_url() 
         for item in site.item_set.all()
     ])
+    # news pages
+    urls.extend([
+        site_name + news.get_absolute_url()
+        for news in site.release_set.all()
+    ])
     xml = loader.render_to_string('sitemap.xml', {'urls': urls})
     return HttpResponse(xml, mimetype='application/xml')
