8b87caaac626998c2f8dbe6c13e3cb2aa833c40e
Jonathan Lukens <jonathan@threadsafelabs.com>
2010-06-10 23:11:09 -0400

    Added password reset stuff.

--- tiger/dashboard/urls.py
+++ tiger/dashboard/urls.py
@@ -18,3 +18,10 @@ urlpatterns += patterns('',
     (r'^look/', include('tiger.dashboard.look.urls')),
     (r'^account/', include('tiger.dashboard.account.urls')),
 )
+
+urlpatterns += patterns('django.contrib.auth.views',
+    (r'^reset-password/$', 'password_reset', {'template_name': 'accounts/password_reset.html', 'email_template_name': 'accounts/password_reset_email.txt'}, 'password_reset'),
+    (r'^reset-password/done/$', 'password_reset_done', {'template_name': 'accounts/password_reset_done.html'}, 'password_reset_done'),
+    (r'^reset-password/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'password_reset_confirm', {'template_name': 'accounts/password_reset_confirm.html'}, 'password_reset_confirm'),
+    (r'^reset-password/complete/$', 'password_reset_complete', {'template_name': 'accounts/password_reset_complete.html'}, 'password_reset_complete'),
+)
