950617f7cccdf79238d11b4ec4a289950ed781f7
Jonathan Lukens <jonathan@threadsafelabs.com>
2010-06-26 00:07:45 -0400

    Further tweaks to search index.

--- tiger/core/search_indexes.py
+++ tiger/core/search_indexes.py
@@ -7,9 +7,11 @@ from tiger.core.models import Item
 class ItemIndex(RealTimeSearchIndex):
     text = CharField(document=True, use_template=True)
     site = IntegerField(model_attr='site__id')
+    item = CharField(model_attr='name')
     section = CharField(model_attr='section__name')
     vegetarian = BooleanField(model_attr='vegetarian')
     spicy = BooleanField(model_attr='spicy')
+    url = CharField(model_attr='get_absolute_url')
 
     def get_queryset(self):
         """Used when the entire index for model is updated."""
