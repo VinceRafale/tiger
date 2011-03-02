59bd8effcfabfe1e30aa2bf91e3909943848c3f3
Jonathan Lukens <jonathan@threadsafelabs.com>
2011-03-01 23:53:17 -0500

    Changed xml generation to reflect current arguments.

--- tiger/sales/authnet.py
+++ tiger/sales/authnet.py
@@ -69,7 +69,7 @@ class Cim(object):
         customer_payment_profile.text = str(customer_payment_profile_id)
         order = etree.SubElement(auth_capture, 'order')
         invoice_number = etree.SubElement(order, 'invoiceNumber')
-        invoice_number.text = invoice.invoiceno
+        invoice_number.text = invoice
         results = self.fetch(xml)
         success = results.messages.resultCode == u'Ok'
         if not success:
