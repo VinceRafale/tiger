99ddb0ad946ddc70b7a9fbf4ca0bc28af52b6266
Jonathan Lukens <jonathan@threadsafelabs.com>
2011-03-03 22:26:56 -0500

    Moved context managers into decorators.

--- tiger/sms/tests/test_automation.py
+++ tiger/sms/tests/test_automation.py
@@ -28,27 +28,27 @@ class AutomatedResponseTestCase(TestCase):
         sender.get_sms_response = Mock(return_value={})
         return sender
 
-    def test_signup_triggers_intro_sms_if_enabled(self):
+    @patch.object(SmsSubscriber, 'sender')
+    def test_signup_triggers_intro_sms_if_enabled(self, mock_method):
         "signup triggers intro sms if enabled"
         self.settings.send_intro = True
         self.settings.save()
         number = faker.phone_number.phone_number()[:20]
-        with patch.object(SmsSubscriber, 'sender') as mock_method:
-            mock_method.return_value = self.get_mock_sender()
-            s = SmsSubscriber.objects.create(settings=self.settings, phone_number=number)
-            self.assertTrue(s.sender.called)
-            sms = SMS.objects.get(subscriber=s)
-            sms.body |should| start_with(self.settings.intro_sms)
-            sms.body |should| end_with(' Reply "out" to quit')
+        mock_method.return_value = self.get_mock_sender()
+        s = SmsSubscriber.objects.create(settings=self.settings, phone_number=number)
+        self.assertTrue(s.sender.called)
+        sms = SMS.objects.get(subscriber=s)
+        sms.body |should| start_with(self.settings.intro_sms)
+        sms.body |should| end_with(' Reply "out" to quit')
 
-    def test_signup_does_not_trigger_intro_sms_if_disabled(self):
+    @patch.object(SmsSubscriber, 'sender')
+    def test_signup_does_not_trigger_intro_sms_if_disabled(self, mock_method):
         "signup does not trigger intro sms if disabled"
         self.settings.send_intro = False
         self.settings.save()
         number = faker.phone_number.phone_number()[:20]
-        with patch.object(SmsSubscriber, 'sender') as mock_method:
-            mock_method.return_value = self.get_mock_sender()
-            s = SmsSubscriber.objects.create(settings=self.settings, phone_number=number)
-            s = SmsSubscriber.objects.create(settings=self.settings, phone_number=number)
-            self.assertFalse(s.sender.called)
-            self.assertRaises(SMS.DoesNotExist, SMS.objects.get, subscriber=s, body=self.settings.intro_sms)
+        mock_method.return_value = self.get_mock_sender()
+        s = SmsSubscriber.objects.create(settings=self.settings, phone_number=number)
+        s = SmsSubscriber.objects.create(settings=self.settings, phone_number=number)
+        self.assertFalse(s.sender.called)
+        self.assertRaises(SMS.DoesNotExist, SMS.objects.get, subscriber=s, body=self.settings.intro_sms)
